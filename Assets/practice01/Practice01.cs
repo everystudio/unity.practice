a7c9e4cc167bee1171382e8e1df51be4d4e40f75
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-12-07 00:37:07 +0900

    練習問題スクリプト追加

---
+++ Assets/practice01/Practice01.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 次のクラスのうち、
+ * メンバー変数はいくつあるでしょう？
+ * */
+public class Practice01 : MonoBehaviour
+{
+
+}
+
+public class Practice01_1 : MonoBehaviour
+{
+    public int m_iAttack;
+    int m_iDefence;
+    void Start()
+    {
+    }
+
+    protected int m_iHp;
+    void Update()
+    {
+    }
+    int m_iMp;
+}
+
+public class Practice01_2 : MonoBehaviour
+{
+    public int m_iAttack;
+    void Start()
+    {
+        int m_iDefence;
+    }
+
+    protected int m_iHp;
+    void Update()
+    {
+        int m_iMp;
+    }
+    int m_iTp;
+}
+
+
+
+
---
+++ Assets/practice02/Practice02.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice02 : MonoBehaviour
+{
+	private int m_iHP;
+
+	public int Damage( int _iAttack)
+	{
+		m_iHP -= _iAttack;
+		if( m_iHP < 0)
+		{
+			m_iHP = 0;
+		}
+		return m_iHP;
+	}
+}
+
+
